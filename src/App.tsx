ee4f46a40963fb63d8b607e97630bb94226f1303
Jfr-N <juann0182@clanyc.org>
2021-02-21 13:10:08 -0500

    Commit!

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Chess from './chess_components/Chess';
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>George dumb</h1>
+    <div id="chess">
+      <Chess/>
     </div>
   );
 }
--- src/Chessboard.tsx/Chess.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './Chess.css';
-
-class Chess extends React.Component{
-    
-}
